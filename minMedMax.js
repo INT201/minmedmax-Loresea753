7499a4fe0647acef0bf4d39835cf5dc8b3d797c6
Loresea753 <woratan.2499@mail.kmutt.ac.th>
2022-09-03 19:37:27 +0700

    v2.0

--- minMedMax.js
+++ minMedMax.js
@@ -4,7 +4,7 @@ function minMedMax(n1, n2, n3) {
   let num =[n1,n2,n3]
   // num.sort()
   num.sort(function(a,b){return a-b})
-  return `{ min: ${num[0]}, mid: ${num[1]}, max: ${num[2]} }`
+  return console.log(`{ min: ${num[0]}, mid: ${num[1]}, max: ${num[2]} }`)
 }
-//  console.log(minMedMax(-1, -8, 0))
+minMedMax(-1, -8, 0)
 module.exports = minMedMax
