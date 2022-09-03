6e6a4875a7c9bb6d026681fab54ae3d4b1cb3285
Loresea753 <woratan.2499@mail.kmutt.ac.th>
2022-09-03 19:17:27 +0700

    test 7pm1.1

--- minMedMax.js
+++ minMedMax.js
@@ -2,8 +2,8 @@ const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
   let num =[n1,n2,n3]
-  num.sort(function(a,b){return b-a})
-  return `max: ${num[0]}, mid: ${num[1]}, min: ${num[2]}`
+  // num.sort(function(a,b){return b-a})
+  return `{mim: ${num[0]}, mid: ${num[1]}, max: ${num[2]}}`
 }
-console.log(minMedMax(85, 30, 1))
+// console.log(minMedMax(85, 30, 1))
 module.exports = minMedMax
